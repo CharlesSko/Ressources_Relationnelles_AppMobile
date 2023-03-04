95852af11a8c360005d19a10f0f0f05cd71cb2bd
CharlesSko <72762628+CharlesSkorupka@users.noreply.github.com>
2023-03-04 21:28:49 +0100

    ConnexionPageFront

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ressources_relationnel/screens/guest/Auth.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Ressources Relationnelles';
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Acueille (Toutes les ressources)',
-      style: optionStyle,
-    ),
-    Text(
-      'Nouvelle Ressource',
-      style: optionStyle,
-    ),
-    Text(
-      'Profil',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ressources Relationnelles'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: 'Acueille',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Nouvelle Ressource',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.perm_identity_outlined),
-            label: 'Profil',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromRGBO(53, 86, 137, 1),
-        onTap: _onItemTapped,
-      ),
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      title: 'Ressource Relationelle',
+      home: const AuthScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/screens/guest/Auth.dart
@@ -0,0 +1,131 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFD8E5F9),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Connexion',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Entrez votre pseudo'),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: 'Ex : Bob',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Color(0xFFA41C61),
+                              ),
+                            )),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text('Entrez votre mots de passe'),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: 'Ex : 1234',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Color(0xFFA41C61),
+                              ),
+                            )),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15.0,
+                          ),
+                          backgroundColor: Color(0xFFA41C61),
+                        ),
+                        onPressed: () =>
+                            print('send'), //Vérification UserName / Password
+                        child: Text(
+                          'Connexion',
+                          style: TextStyle(
+                            color: Color(0xFFD8E5F9),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15.0,
+                          ),
+                          backgroundColor: Color(0xFFA41C61),
+                        ),
+                        onPressed: () =>
+                            print('send'), //Vérification UserName / Password
+                        child: Text(
+                          'Inscription',
+                          style: TextStyle(
+                            color: Color(0xFFD8E5F9),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
